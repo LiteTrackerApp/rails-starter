402c8df34ef236348d415a543b717bd36f7429fe
Aleksandar M <sale@kolosek.com>
2026-02-13 10:09:24 +0100

    [#444054] Fix issue with action cable (4)

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module RailsTabler8
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_job.queue_adapter = :inline
   end
 end
